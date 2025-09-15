67ddaa10464195988de41691e01bc52d7f0c1e92
bungroti <ratthavit.big@icloud.com>
2025-09-15 13:33:18 +0700

    Add files via upload

---
+++ coding_w09_01.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int start, stop;
+    
+    printf("Enter start number: ");
+    scanf("%d", &start);
+    printf("Enter stop number: ");
+    scanf("%d", &stop);
+
+    if (start < stop) {
+        printf("Even numbers from %d to %d are:\n", start, stop);
+        for (int i = start; i <= stop; i++) {
+            if (i % 2 == 0) {
+                printf("%d ", i);
+            }
+        }
+        printf("\nEnd of loop\n");
+    }
+    else if (start == stop) {
+        printf("Your Start number is equal to Stop number, please try again!\n");
+    }
+    else { // start > stop
+        printf("Your Start number is greater than Stop number, please try again!\n");
+    }
+
+    return 0;
+}
+//  โค้ดนี้ทำงานตามเงื่อนไขที่โจทย์กำหนด (ตรวจสอบ start < stop, = stop, > stop)
+//  ถ้า start < stop จะแสดงเลขคู่ตามปกติ
+//  ถ้า start = stop หรือ start > stop จะแจ้งเตือนและไม่แสดงผล
